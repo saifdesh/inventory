fe9a4fc484b9db607d9b987156a1728fdf9e0c95
saifdesh <saifurs.87@gmail.comm>
2022-05-09 19:41:04 +0600

    update login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import app from '../../firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { Button } from 'react-bootstrap';
+import img from '../../google.png';
+
+
+const auth = getAuth(app);
 
 const Login = () => {
+    const [user, setUser] = useState({})
+    const provider = new GoogleAuthProvider()
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <div>
             <h2>this login</h2>
+
+            <Button onClick={handleGoogleSignIn}>
+                <img src={img} /> Google sign in</Button>
+            <p>Name: {user.displayName}</p>
+            <img src={user.photoURL} height="30" width="30" />
 
 
         </div>
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSzW7CtXO_ZPty8xSqH3W0siGpsBLNGrc",
+    authDomain: "inventory-f2bb0.firebaseapp.com",
+    projectId: "inventory-f2bb0",
+    storageBucket: "inventory-f2bb0.appspot.com",
+    messagingSenderId: "746467382813",
+    appId: "1:746467382813:web:7f151fa4852efa6ba958f4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
